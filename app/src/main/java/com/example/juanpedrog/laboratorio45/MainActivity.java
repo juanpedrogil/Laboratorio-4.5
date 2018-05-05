bee4b878e2c689b463fee28ea5d584b1f157fd62
Juan Pedro Gil Llanos <jupegilll@ittepic.edu.mx>
2018-05-04 18:35:29 -0700

    Primer commit

---
+++ app/src/main/java/com/example/juanpedrog/laboratorio45/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.juanpedrog.laboratorio45;
+
+import android.animation.Animator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.faceIcon);
+    }
+
+    public void onButtonClick(View v) {
+
+        imageView.animate()
+                .scaleX(0.5f)
+                .scaleY(0.5f)
+                .rotationX(360)
+                .rotationY(360)
+                .scaleX(1f)
+                .scaleY(1f)
+                //.translationX(200)
+                //.translationY(200)
+                .setDuration(3000)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        Log.i("Animation", "onAnimationStart");
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        Log.i("Animation", "onAnimationEnd");
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+    }
+
+}
